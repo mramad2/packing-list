4ea3f542211916c4c93dd49a6fec5e522ce9aa5c
fvonhoven <frank@infinite.red>
2018-07-21 12:01:37 -0500

    Drawer POC & add TSlint

--- src/app/main.js
+++ src/app/main.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from "react-native"
 import RootComponent from "../app/root-component"
+console.disableYellowBox = true
 
 AppRegistry.registerComponent("PackingList", () => RootComponent)
--- src/app/root-component.js
+++ src/app/root-component.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react"
 import { PackingListScreen } from "../screens/packing-list-screen"
+import { createStackNavigator, createDrawerNavigator } from "react-navigation"
+import { Text } from "react-native"
 
-/*
-  STEP FIVE
-  • Setup basic project folder structure
-*/
+const DrawerStack = createDrawerNavigator(
+  {
+    screen1: createStackNavigator({
+      Home: {
+        screen: PackingListScreen,
+        navigationOptions: ({ navigation }) => ({
+          title: `Nav Title`,
+          headerLeft: (
+            <Text style={{ marginLeft: 10 }} onPress={() => navigation.toggleDrawer()}>
+              HEY
+            </Text>
+          )
+        })
+      }
+    })
+  },
+  { drawerWidth: 250 }
+)
 
 export default class RootComponent extends Component {
-    render() {
-        return <PackingListScreen />
-    }
+  render() {
+    return <DrawerStack />
+  }
 }
