42409cc14ca11c0208bbb796450fa89fdbf89978
fvonhoven <frank@infinite.red>
2018-08-17 11:41:54 -0500

    Write tests & add snapshot tests

--- __tests__/button.test.js
+++ __tests__/button.test.js
@@ -1,8 +1,22 @@
 import React from "react"
-// import { Button } from "../src/components/button"
-// import renderer from "react-test-renderer"
+import { Button } from "../src/components/button"
+import renderer from "react-test-renderer"
 
 test("button has onPress fn", () => {
-  // const buttonInstance = renderer.create(<Button />).getInstance()
-  expect(true).toBe(true)
+  let x = 0
+  const buttonInstance = renderer.create(<Button onPress={() => x++} />).getInstance()
+  expect(buttonInstance.props.onPress).toBeDefined()
+  expect(x).toBe(0)
+  buttonInstance.props.onPress()
+  expect(x).toBe(1)
+})
+
+test("Button shape has not changed", () => {
+  const snapshot = renderer.create(<Button />).toJSON()
+  expect(snapshot).toMatchSnapshot()
+})
+
+test("Button bg is grey on clear prop", () => {
+  const snapshotClear = renderer.create(<Button clear />).toJSON()
+  expect(snapshotClear).toMatchSnapshot()
 })
--- tests/button.test.js
+++
@@ -1,7 +0,0 @@
-// import { Button } from "../src/components/button"
-// import renderer from "react-test-renderer"
-
-test("button has onPress fn", () => {
-  // const buttonInstance = renderer.create(<Button />).getInstance()
-  expect(true).toBe(true)
-})
